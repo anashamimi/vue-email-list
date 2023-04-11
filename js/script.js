377603556e5fc7a41416a23d83b188da79ac12eb
anashamimi <anashamimi2@gmail.com>
2023-04-11 19:59:56 +0200

    visualizzazione delle 10 mail

--- js/script.js
+++ js/script.js
@@ -2,9 +2,29 @@ const { createApp } = Vue;
 
 createApp({
     data() {
-        return {            
+        return {
+            randomMail: [],
+            basePath: 'https://flynn.boolean.careers/exercises/api/',
         }
     },
-    methods: {        
-    },    
+    methods: {
+        getData() {
+            this.randomMail = [];
+
+            for (let i = 0; i < 10; i++) {
+
+                axios.get(this.basePath + 'random/mail').then((res) => {
+                    console.log(res.data.response);
+                    this.randomMail.push(res.data.response);
+                });
+            }
+
+
+
+        }
+    },
+    mounted() {
+        this.getData();
+    }
 }).mount('#app');
+
